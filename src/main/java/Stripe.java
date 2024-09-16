c330c443bec3cce272eb854c201e32cbf54417ee
ayush-020198 <ayush.shrivastavaujn@gmail.com>
2024-09-16 15:23:46 +0530

    first commit

---
+++ src/main/java/Stripe.java
@@ -0,0 +1,14 @@
+class Main {
+   public static void main(String[] args) {
+      Stripe stripe = new Stripe();
+      stripe.run();
+   }
+}
+
+class Stripe {
+   public void run() {
+      System.out.println("Stripe");
+   }
+}
+
+
